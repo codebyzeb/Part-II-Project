7032107b382ea09f040fe372fc5fabdf208d9168
zebulon <zg258@cam.ac.uk>
2019-11-12 12:52:42 +0000

    Add Tests for Manual and Neural Entities

---
+++ simulating/tests/test_manual_entity.py
@@ -0,0 +1,59 @@
+"""
+This module runs all the tests for the ManualEntity class
+"""
+
+import simulating.entity as entity
+
+
+def test_behaviour_forwards():
+    """
+    This tests that the manual behaviour returns a forwards action when
+    the input location is 0.
+    """
+
+    ent = entity.ManualEntity()
+    action, _ = ent.behaviour(0)
+    assert action == entity.Action.FORWARDS
+
+
+def test_behaviour_right():
+    """
+    This tests that the manual behaviour returns a right action when
+    the input location is between 0.25 and 0.5
+    """
+
+    ent = entity.ManualEntity()
+    action, _ = ent.behaviour(0.375)
+    assert action == entity.Action.RIGHT
+
+
+def test_behaviour_left():
+    """
+    This tests that the manual behaviour returns a left action when
+    the input location is between 0.25 and 0.5
+    """
+
+    ent = entity.ManualEntity()
+    action, _ = ent.behaviour(0.785)
+    assert action == entity.Action.LEFT
+
+
+def test_behaviour_behind():
+    """
+    This tests that the manual behaviour returns either left or
+    right action when the input location is exatly 0.5
+    """
+
+    ent = entity.ManualEntity()
+    action, _ = ent.behaviour(0.5)
+    assert action in (entity.Action.LEFT or action, entity.Action.RIGHT)
+
+
+def test_behaviour_output_vocal():
+    """
+    This tests that the manual behaviour returns an empty vocal response.
+    """
+
+    ent = entity.ManualEntity()
+    _, vocal = ent.behaviour(0.5)
+    assert vocal == (0, 0, 0)
---
+++ simulating/tests/test_neural_entity.py
@@ -0,0 +1,92 @@
+"""
+This module runs all the tests for the Neural Entity class
+"""
+
+import numpy as np
+
+from simulating.action import Action
+import simulating.entity as entity
+
+
+def test_sigmoid():
+    """
+    Test sigmoid activation function on random array from normal distribution
+    """
+
+    x = np.random.randn(100)
+    y = entity.sigmoid(x)
+    assert (y > 0).all() and (y <= 1).all()
+
+
+def test_relu():
+    """
+    Test relu activation function on random array from normal distribution
+    """
+
+    x = np.random.randn(100)
+    y = entity.relu(x)
+    assert (y >= 0).all()
+
+
+def test_bits_to_array():
+    """
+    Test conversion from mushroom to array
+    """
+
+    mushroom = 0b0000011111
+    mush_array = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+    assert entity.bits_to_array(mushroom, 10) == mush_array
+
+
+def test_array_to_bits():
+    """
+    Test conversion from mushroom to array
+    """
+
+    mushroom = 0b0000011111
+    mush_array = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+    assert entity.array_to_bits(mush_array) == mushroom
+
+
+def test_initialise_parameters_size():
+    """
+    Test that parameters in the neural network for
+    NeuralEntity are set with the right size
+    """
+
+    ent = entity.NeuralEntity(0, [5])
+    print(ent.parameters["W1"].shape)
+    assert len(ent.parameters) == 4
+    assert ent.parameters["W1"].shape == (5, 14)
+    assert ent.parameters["b1"].shape == (5, 1)
+    assert ent.parameters["W2"].shape == (5, 5)
+    assert ent.parameters["b2"].shape == (5, 1)
+
+
+def test_forward_propogation_cache_size():
+    """
+    Test that forward propogation through the entity's
+    neural network returns a cache with appropriate weights
+    """
+
+    ent = entity.NeuralEntity(0, [5])
+    inputs = np.array([0.5, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1])
+    inputs = np.expand_dims(inputs, 1)
+    cache = ent.forward_propagation(inputs, linear=False)
+    assert len(cache) == 5
+    assert np.array_equal(cache['A0'], inputs)
+    assert (cache['A1'] >= 0).all()
+    assert (cache['A2'] >= 0).all() and (cache['A2'] < 1).all()
+
+
+def test_behaviour_output_correct():
+    """
+    Test that the behaviour call outputs an action and a 3-bit vocal call
+    """
+
+    ent = entity.NeuralEntity(0, [5])
+    action, vocal = ent.behaviour(0.4, 0b1111100000, 3)
+    assert action in [
+        Action.FORWARDS, Action.LEFT, Action.RIGHT, Action.NOTHING
+    ]
+    assert 0 <= vocal < 8
